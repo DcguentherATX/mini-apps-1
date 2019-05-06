17b6285e3ab176bf19f0363b234d438e8a365ab9
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-06 14:19:30 -0500

    click function allows X to show in squares

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,23 @@
+let board = document.getElementsByClassName("square")
 
+console.log(board);
+// [
+//     [0, 0, 0],
+//     [0, 0, 0],
+//     [0, 0, 0]
+// ];
 
+// document.addEventListener('click', function(event) {
+//     if (!event.target.matches('.square')) {
+//         return;
+//     }
+//     event.preventDefault();
+//     console.log(event.target);
+// })
+
+updateSquareValue = function() {
+    let box = this.event.target.id;
+    document.getElementById(box).innerHTML = "X"
+}
+
+console.log('app is connected');
