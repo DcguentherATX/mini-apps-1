8be5bd2b33fffc7da9e83acf042600844f9d0e21
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-07 13:47:55 -0500

    completed Tic Tac Toe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,6 +5,19 @@ let active = true;
 let xWins = 0;
 let oWins = 0;
 let prevWinner = '';
+let playerOne = 'X';
+let playerTwo = 'O';
+
+const setName = function() {
+    if (document.getElementById('player1').value) {
+      playerOne = document.getElementById('player1').value;
+      document.getElementById('xwins').innerHTML = playerOne;
+    }
+    if (document.getElementById('player2').value) {
+      playerTwo = document.getElementById('player2').value;
+      document.getElementById('owins').innerHTML = playerTwo;
+    }
+}
 
 const updateSquareValue = function() {
     if (active) {
@@ -13,7 +26,7 @@ const updateSquareValue = function() {
             alert('spot has been claimed');
             return;
         }
-    if (prevWinner === '' || prevWinner === 'O') {
+    if (prevWinner === '' || prevWinner === playerTwo) {
         if(counter % 2 === 1) {
             document.getElementById(box).innerHTML = "X";
             counter++;
@@ -64,7 +77,6 @@ const checkForWinner = function() {
     checkRows();
     checkColumns();
     checkDiagonals();
-
 }
 
 const checkRows = function() {
@@ -73,20 +85,20 @@ const checkRows = function() {
     let rowThree = [board[6].innerHTML, board[7].innerHTML, board[8].innerHTML];
 
     if (rowOne.every(checkX) || rowTwo.every(checkX) || rowThree.every(checkX)) {
-        document.getElementById('victory').innerHTML = "X WINS!  O IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerOne} WINS!  ${playerTwo} IS A FOOL!`;
         active = false;
         xWins++;
-        document.getElementById('xwins').innerHTML = `X Wins: ${xWins}`;
-        prevWinner = 'X';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, O goes first`;
+        document.getElementById('xwins').innerHTML = `${playerOne} Wins: ${xWins}`;
+        prevWinner = `${playerOne}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerTwo} goes first`;
     };
     if (rowOne.every(checkO) || rowTwo.every(checkO) || rowThree.every(checkO)) {
-        document.getElementById('victory').innerHTML = "O WINS!  X IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerTwo} WINS!  ${playerOne} IS A FOOL!`;
         active = false;
         oWins++;
-        document.getElementById('owins').innerHTML = `O Wins: ${oWins}`;
-        prevWinner = 'O';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, X goes first`;
+        document.getElementById('owins').innerHTML = `${playerTwo} Wins: ${oWins}`;
+        prevWinner = `${playerTwo}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerOne} goes first`;
     };
 };
 
@@ -96,20 +108,20 @@ const checkColumns = function() {
     let colThree = [board[2].innerHTML, board[5].innerHTML, board[8].innerHTML];
 
     if (colOne.every(checkX) || colTwo.every(checkX) || colThree.every(checkX)) {
-        document.getElementById('victory').innerHTML = "X WINS!  O IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerOne} WINS!  ${playerTwo} IS A FOOL!`;
         active = false;
         xWins++;
-        document.getElementById('xwins').innerHTML = `X Wins: ${xWins}`;
-        prevWinner = 'X';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, O goes first`;
+        document.getElementById('xwins').innerHTML = `${playerOne} Wins: ${xWins}`;
+        prevWinner = `${playerOne}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerTwo} goes first`;
     };
     if (colOne.every(checkO) || colTwo.every(checkO) || colThree.every(checkO)) {
-        document.getElementById('victory').innerHTML = "O WINS!  X IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerTwo} WINS!  ${playerOne} IS A FOOL!`;
         active = false;
         oWins++
-        document.getElementById('owins').innerHTML = `O Wins: ${oWins}`;
-        prevWinner = 'O';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, X goes first`;
+        document.getElementById('owins').innerHTML = `${playerTwo} Wins: ${oWins}`;
+        prevWinner = `${playerTwo}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerOne} goes first`;
     };
 };
 
@@ -118,20 +130,20 @@ const checkDiagonals = function() {
     let diagBottom = [board[6].innerHTML, board[4].innerHTML, board[2].innerHTML];
 
     if (diagTop.every(checkX) || diagBottom.every(checkX)) {
-        document.getElementById('victory').innerHTML = "X WINS!  O IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerOne} WINS!  ${playerTwo} IS A FOOL!`;
         active = false;
         xWins++;
-        document.getElementById('xwins').innerHTML = `X Wins: ${xWins}`;
-        prevWinner = 'X';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, O goes first`;
+        document.getElementById('xwins').innerHTML = `${playerOne} Wins: ${xWins}`;
+        prevWinner = `${playerOne}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerTwo} goes first`;
     };
     if (diagTop.every(checkO) || diagBottom.every(checkO)) {
-        document.getElementById('victory').innerHTML = "O WINS!  X IS A FOOL!";
+        document.getElementById('victory').innerHTML = `${playerTwo} WINS!  ${playerOne} IS A FOOL!`;
         active = false;
         oWins++;
-        document.getElementById('owins').innerHTML = `O Wins: ${oWins}`;
-        prevWinner = 'O';
-        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner}, X goes first`;
+        document.getElementById('owins').innerHTML = `${playerTwo} Wins: ${oWins}`;
+        prevWinner = `${playerTwo}`;
+        document.getElementById('prevwinner').innerHTML = `Previous Winner: ${prevWinner} ... ${playerOne} goes first`;
     };
 };
 
