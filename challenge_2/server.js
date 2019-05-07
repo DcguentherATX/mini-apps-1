d9fbd5a471dbb92e0cdb4743675ec64b75b78085
DAVID GUENTHER <davidguenther@DAVIDs-MacBook-Pro.local>
2019-05-07 17:42:46 -0500

    removed commas from beginning and end of lines

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -7,18 +7,40 @@ app.use(express.static('client'));
 app.use(bodyParser());
 
 app.get('/', function (req, res) {
+    console.log('GET')
     res.send('Hello World!')}
     );
 
 app.post('/', function (req, res) {
     let wreck = req.body;
     let valArr = Object.values(wreck);
-    //console.log('values', valArr);
-    let stringed = valArr.toString();
-    stringed.replace(/(\r\n|\n|\r)/gm,"");
-    console.log('valor', JSON.parse(stringed));
-    res.send()}
-    )
+    let results = [];
+    let parsedData = JSON.parse(valArr);
+    let keys = Object.keys(parsedData);
+    keys.pop();
+    keys.map((key) => results.push(key));
+    results.push('\n');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    var nested = function(obj) {
+        for (var key in obj) {
+            if (key !== 'children') {
+                results.push(obj[key]);
+            } else if (key === 'children' && obj.children.length > 0) {
+                for (var i = 0; i < obj.children.length; i++) {
+                    results.push('\n');
+                    nested(obj.children[i]);
+                }
+            }
+        }
+    }
+    nested(parsedData);
+    let joined = results.join();
+    
+    var str2 = joined.replace(/^,+/gm, '');
+    var str3 = str2.replace(/,\s*$/gm, "");
+    console.log(str3);
+    res.send(str3);
+});
+
+app.listen(port, () => console.log(`CSV Generator listening on port ${port}!`))
 
